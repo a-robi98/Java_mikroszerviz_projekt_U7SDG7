409701336ac6f3c305f9c87b95e588127f382e3c
a-robi98 <robogosims@gmail.com>
2023-06-05 22:05:43 +0200

    Created microservice project

---
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/JavaMicroserviceProjectU7Sdg7Application.java
@@ -0,0 +1,13 @@
+package com.example.java__microservice_project_u7sdg7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroserviceProjectU7Sdg7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMicroserviceProjectU7Sdg7Application.class, args);
+    }
+
+}
---
+++ Java__microservice_project_U7SDG7/src/test/java/com/example/java__microservice_project_u7sdg7/JavaMicroserviceProjectU7Sdg7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java__microservice_project_u7sdg7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroserviceProjectU7Sdg7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
