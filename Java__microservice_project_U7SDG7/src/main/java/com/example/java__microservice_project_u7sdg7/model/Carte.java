1e9ea9e0cc6ffbc432b104e79304f1aa63a9921f
a-robi98 <robogosims@gmail.com>
2023-06-06 21:22:20 +0200

    Added additional changes to model classes

--- Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/model/Carte.java
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/model/Carte.java
@@ -1,4 +1,33 @@
 package com.example.java__microservice_project_u7sdg7.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Carte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String carteName;
+    private String type;
+    private String description;
+
+    @Builder
+    public Carte(String carteName, String type, String description) {
+        this.id = id;
+        this.carteName = carteName;
+        this.type = type;
+        this.description = description;
+    }
+
+
 }
--- Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/model/CarteItem.java
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/model/CarteItem.java
@@ -1,4 +1,33 @@
 package com.example.java__microservice_project_u7sdg7.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class CarteItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int carteId;
+    private String name;
+    private String type;
+    private int price;
+
+    @Builder
+    public CarteItem( String name, String type, int price)
+    {
+        this.carteId = carteId;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
 }
