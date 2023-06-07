46d774b8033862eb874386a98d1d9f7b80beda8b
a-robi98 <robogosims@gmail.com>
2023-06-07 14:26:12 +0200

    Changed repositoreis

--- Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/repository/CarteRepository.java
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/repository/CarteRepository.java
@@ -3,13 +3,14 @@ package com.example.java__microservice_project_u7sdg7.repository;
 import com.example.java__microservice_project_u7sdg7.model.Carte;
 import com.example.java__microservice_project_u7sdg7.model.CarteItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+//@Repository
 public interface CarteRepository extends JpaRepository<Carte, Long> {
-    List<CarteItem> listAllCarte();
+  /*  List<CarteItem> listAllCarte();
     CarteItem getCarteById(Long id);
-    void insertCarte(CarteItem carteItem);
+    void insertCarte(CarteItem carteItem);*/
 }
