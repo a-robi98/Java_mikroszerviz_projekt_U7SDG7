3f855b26fdb6acb1d84f0744473b2d3483adfb24
a-robi98 <robogosims@gmail.com>
2023-06-07 14:26:29 +0200

    added controllers

---
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteController.java
@@ -0,0 +1,5 @@
+package com.example.java__microservice_project_u7sdg7.controller;
+
+public class CarteController {
+
+}
---
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteItemController.java
@@ -0,0 +1,4 @@
+package com.example.java__microservice_project_u7sdg7.controller;
+
+public interface CarteItemController {
+}
