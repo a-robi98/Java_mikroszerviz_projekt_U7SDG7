83fe9068ba92c068a75e3d3daa93be97b1558b83
a-robi98 <robogosims@gmail.com>
2023-06-08 00:15:43 +0200

    wrote controllers code

--- Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteController.java
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteController.java
@@ -1,5 +1,44 @@
 package com.example.java__microservice_project_u7sdg7.controller;
 
+import com.example.java__microservice_project_u7sdg7.model.Carte;
+import com.example.java__microservice_project_u7sdg7.service.CarteService;
+import com.example.java__microservice_project_u7sdg7.service.CarteServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CarteController {
 
+    private final CarteServiceImpl carteServiceImpl;
+
+    public CarteController(CarteServiceImpl carteServiceImpl) {
+        this.carteServiceImpl = carteServiceImpl;
+    }
+
+    @GetMapping("/carte/list")
+    public ResponseEntity<List<Carte>> getCarteList() {
+        return new ResponseEntity<>(this.carteServiceImpl.getCarteList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/carte/id")
+    public ResponseEntity<Carte> getCarteById(@RequestParam Long id) {
+        return new ResponseEntity<>(this.carteServiceImpl.getCarteById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/carte/name")
+    public ResponseEntity<Carte> getCarteByName(@RequestParam String name) {
+        return new ResponseEntity<>(this.carteServiceImpl.getCarteByCarteName(name), HttpStatus.OK);
+    }
+
+    @PostMapping("/carte/addcarte")
+    public ResponseEntity<Carte> addCarte(@RequestParam Carte carte) {
+        this.carteServiceImpl.addCarte(carte);
+        return new ResponseEntity<>(carte,HttpStatus.CREATED);
+    }
 }
--- Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteItemController.java
+++ Java__microservice_project_U7SDG7/src/main/java/com/example/java__microservice_project_u7sdg7/controller/CarteItemController.java
@@ -1,4 +1,44 @@
 package com.example.java__microservice_project_u7sdg7.controller;
 
-public interface CarteItemController {
+import com.example.java__microservice_project_u7sdg7.model.CarteItem;
+import com.example.java__microservice_project_u7sdg7.service.CarteItemServiceImpl;
+import com.example.java__microservice_project_u7sdg7.service.CarteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarteItemController {
+
+    private final CarteItemServiceImpl carteItemServiceImpl;
+
+    public CarteItemController(CarteItemServiceImpl carteItemServiceImpl) {
+        this.carteItemServiceImpl = carteItemServiceImpl;
+    }
+
+    @GetMapping("/carteitem/list")
+    public ResponseEntity<List<CarteItem>> getCarteList() {
+        return new ResponseEntity<>(this.carteItemServiceImpl.getCarteItemList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/carteitem/id")
+    public ResponseEntity<CarteItem> getCarteById(@RequestParam Long id) {
+        return new ResponseEntity<>(this.carteItemServiceImpl.getCarteItemById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/carteitem/name")
+    public ResponseEntity<CarteItem> getCarteByName(@RequestParam String name) {
+        return new ResponseEntity<>(this.carteItemServiceImpl.getCarteItemByName(name), HttpStatus.OK);
+    }
+
+    @PostMapping("/carte/addcarteitem")
+    public ResponseEntity<CarteItem> addCarte(@RequestParam CarteItem carteItem) {
+        this.carteItemServiceImpl.addCarteItem(carteItem);
+        return new ResponseEntity<>(carteItem,HttpStatus.CREATED);
+    }
 }
